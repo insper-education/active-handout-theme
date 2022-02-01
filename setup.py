acf1864e1149260fcb64a99c6d290d4e8dd20146
Andrew Kurauchi <andrew.kurauchi@gmail.com>
2022-01-31 22:10:51 -0300

    Extract theme from overrides

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+with open("requirements.txt") as data:
+    install_requires = [
+        line for line in data.read().split("\n")
+        if line and not line.startswith("#")
+    ]
+
+setup(
+    name="mkdocs-active-handout",
+    version=VERSION,
+    url='https://github.com/insper-education/active-handout-theme',
+    license='',
+    description="Mkdocs theme for Insper's active handouts",
+    author='Igor Montagner',
+    author_email='igordsm@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points={
+        'mkdocs.themes': [
+            'active-handout = active_handout',
+        ]
+    },
+    zip_safe=False
+)
